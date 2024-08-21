f53d28e1ba0ab3c339bf9d078d8589794d18592d
Ezequiel <ezee7771@gmail.com>
2024-08-20 22:38:04 -0300

    Se suben los ejercicios de la clase 2

---
+++ ejercicio-3.js
@@ -0,0 +1,33 @@
+
+// separarPorPalabrasPares
+
+// Estrucutra del resultado
+
+// resultado = {
+//     pares:[...],
+//     impares:[...]:
+// }
+
+
+function separarPorPares(array) {
+    let resultado = {
+        pares: [],
+        impares: []
+    };
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length % 2 === 0) {
+            resultado.pares.push(array[i]);
+        } else {
+            resultado.impares.push(array[i]);
+        }
+    }
+
+    return resultado;
+}
+
+let palabras = ['Auto', 'Parlante', 'Moto', 'Sal', 'Perfume', 'estrella', 'Sol', 'Nubes'];
+
+const resultado = separarPorPares(palabras);
+
+console.log(resultado);
